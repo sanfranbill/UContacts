8560dcf141fdb7b71720f17e4c137051590bf3b6
Bill Tang <laptopbatteryusa@gmail.com>
2018-05-13 22:52:43 +0800

    Updated to pass data into components with props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map((person) => (
-          <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
+const contacts = [
+ {
+   "id": "karen",
+   "name": "Karen Isgrigg",
+   "handle": "karen_isgrigg",
+   "avatarURL": "http://localhost:5001/karen.jpg"
+ },
+ {
+   "id": "richard",
+   "name": "Richard Kalehoff",
+   "handle": "richardkalehoff",
+   "avatarURL": "http://localhost:5001/richard.jpg"
+ },
+ {
+   "id": "tyler",
+   "name": "Tyler McGinnis",
+   "handle": "tylermcginnis",
+   "avatarURL": "http://localhost:5001/tyler.jpg"
+ }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Tyler'},
-          { name: 'Karen'},
-          { name: 'Richard'}
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Amanda'},
-          { name: 'Mikenzi'},
-          { name: 'Ryan'}
-        ]}/>
+      <div>
+        <ListContacts contacts={contacts} />
       </div>
-    );
+    )
   }
 }
 
